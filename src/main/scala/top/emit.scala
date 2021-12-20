6bd8d367c67edf9f7e92be5036be0c0165ead065
Fangzhou Yuan <yfzcsc@gmail.com>
2021-12-20 23:31:54 +0800

    delete output files

--- src/main/scala/top/emit.scala
+++ src/main/scala/top/emit.scala
@@ -7,45 +7,27 @@ import chisel3.experimental.BundleLiterals._
 
 import chisel3.stage.ChiselStage
 
+// test
+
 class Fuck extends Module{
     val io = IO(new Bundle{
-        val f = Output(Bool())
+        val sbx = Input(SInt(18.W))
+        val rbx = Input(SInt(40.W))
+        val bx = Input(SInt(18.W))
+        val by = Input(SInt(18.W))
+        val ax = Input(SInt(20.W))
+        val ay = Input(SInt(20.W))
+        val x = Output(SInt(40.W))
+        val y = Output(SInt(40.W))
     })
-    val A = Module(new big_bank0(10, 20)).io
-    val x = RegInit(0.U.asTypeOf(ACounter(6.W)))
-    val state = RegInit(0.U(6.W))
-    io.f := false.B
-    switch(state){
-        is(0.U){
-            x.set(10.U)
-            state := 1.U
-        }
-        is(1.U){
-            when(x.inc()){
-                state := 2.U
-            }.otherwise{
-                A.addra := x.ccnt
-                A.dina := x.ccnt    
-            }
-            
-        }
-        is(2.U){
-            when(x.inc()){
-                state := 3.U
-            }.otherwise{
-                A.addrb := x.ccnt
-                when(A.doutb=/=x.ccnt){
-                    state := 4.U
-                }
-            }
-        }
-        is(3.U){
-            io.f := true.B
-        }
-        is(4.U){
-            io.f := false.B
-        }
-    }
+    val _ax = RegNext(io.ax, 0.S(18.W))
+    val _ay = RegNext(io.ay, 0.S(18.W))
+    val _bx = RegNext(io.bx, 0.S(18.W))
+    val _by = RegNext(io.by, 0.S(18.W))
+    val w = RegInit(0.S(40.W))
+    w := (io.ay<<(1.U))*io.sbx+&io.rbx
+    io.x := (_ax+&_ay)*_bx-&w
+    io.y := (_ax-&_ay)*_by-&w
 }
 
 object FafaDriver extends App {
@@ -54,5 +36,14 @@ object FafaDriver extends App {
         //new ROMBias,
         //new RAMGroup(StdPara.w, StdPara.addr_w, StdPara.id_w), 
         Array("--target-dir", "C:\\Users\\yfz\\Desktop\\data\\Plan\\homework\\AI_fpga\\final_project\\project_1\\")
+    )
+}
+
+object FuckDriver extends App {
+    (new ChiselStage).emitVerilog(
+        new Fuck,
+        //new ROMBias,
+        //new RAMGroup(StdPara.w, StdPara.addr_w, StdPara.id_w), 
+        Array("--target-dir", "C:\\Users\\yfz\\Desktop\\data\\Plan\\homework\\AI_fpga\\final_project\\project_test\\")
     )
 }
